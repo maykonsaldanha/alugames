142ea14a1933db0a7f604a3eaff4695e72f495a3
maykonsaldanha <maykon.saldanha@agroamazonia.com>
2025-02-16 16:49:37 -0400

    adicionando mudanças

--- js/app.js
+++ js/app.js
@@ -11,15 +11,15 @@ function alterarStatus(id){
     gameBotao.textContent = 'Alugar'
     gameBotao.classList.remove('dashboard__item__button--return');
 
-    if(confirm('Tem certeza que deseja devolver o livro?')){ //Opção de cancelar
-    gameImagem.classList.remove('dashboard__item__img--rented');
-    gameBotao.textContent = 'Alugar'
-    gameBotao.classList.remove('dashboard__item__button--return')
-        quantAluguelJogos--; //Reduz o valor
-    }else{
-    gameImagem.classList.add('dashboard__item__img--rented'); // A imagem está clara, ao clicar adiciono o tema escuro
-    gameBotao.textContent = 'Devolver'
-    gameBotao.classList.add('dashboard__item__button--return')
+        if(confirm('Tem certeza que deseja devolver o livro?')){ //Opção de cancelar
+        gameImagem.classList.remove('dashboard__item__img--rented');
+        gameBotao.textContent = 'Alugar'
+        gameBotao.classList.remove('dashboard__item__button--return')
+            quantAluguelJogos--; //Reduz o valor
+        }else{
+        gameImagem.classList.add('dashboard__item__img--rented'); // A imagem está clara, ao clicar adiciono o tema escuro
+        gameBotao.textContent = 'Devolver'
+        gameBotao.classList.add('dashboard__item__button--return')
     }
     
    }else {
